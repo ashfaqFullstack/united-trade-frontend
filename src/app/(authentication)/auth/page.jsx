1049097c77a522e2853d296e085b9d14aef6f0e2
ashfaqFullstack <ashfaqahmadfullstack@gmail.com>
2026-09-07 09:35:46 +0500

    auth protected

--- src/app/(authentication)/auth/page.jsx
+++ src/app/(authentication)/auth/page.jsx
@@ -1,9 +1,12 @@
+import RouteGuard from "@/route/guard/RouteGuard";
 import AuthCard from "./_components/AuthCard";
 
 export default function AuthPage() {
     return (
-        <main className="flex min-h-screen items-center justify-center bg-[#F4F6FF] p-6">
-            <AuthCard />
-        </main>
+        <RouteGuard type="guest">
+            <main className="flex min-h-screen items-center justify-center bg-[#F4F6FF] p-6">
+                <AuthCard />
+            </main>
+        </RouteGuard>
     );
 }
